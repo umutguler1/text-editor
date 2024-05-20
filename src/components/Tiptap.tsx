14bd1645eb307e9719ed516d395a5c4b0adbee8c
Umut Güler <umtglr1121@gmail.com>
2024-05-20 17:32:00 +0300

    tiptap updates

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,74 +1,28 @@
 "use client";
 
 import React from "react";
-import { useEditor, EditorContent } from "@tiptap/react";
-import { StarterKit } from "@tiptap/starter-kit";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 import Image from "@tiptap/extension-image";
 import TextAlign from "@tiptap/extension-text-align";
-import Underline from "@tiptap/extension-underline";
-
-const MenuBar = ({ editor }: any) => {
-  if (!editor) {
-    return null;
-  }
-
-  const addImage = () => {
-    const url = window.prompt("URL");
-
-    if (url) {
-      editor.chain().focus().setImage({ src: url }).run();
-    }
-  };
-
-  return (
-    <div className="flex bg-indigo-100 w-full justify-between font-semibold">
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-      >
-        Bold
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-      >
-        Italic
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleUnderline().run()}
-        disabled={!editor.can().chain().focus().toggleUnderline().run()}
-      >
-        Underline
-      </button>
-      <button onClick={addImage}>Add Image</button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("left").run()}
-        disabled={!editor.can().chain().focus().setTextAlign("left").run()}
-      >
-        Left Align
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("center").run()}
-        disabled={!editor.can().chain().focus().setTextAlign("center").run()}
-      >
-        Center Align
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("right").run()}
-        disabled={!editor.can().chain().focus().setTextAlign("right").run()}
-      >
-        Right Align
-      </button>
-    </div>
-  );
-};
+import Link from "@tiptap/extension-link";
+import {
+  FaBold,
+  FaItalic,
+  FaUnderline,
+  FaImage,
+  FaAlignLeft,
+  FaAlignCenter,
+  FaAlignRight,
+} from "react-icons/fa";
+// import "./TiptapEditor.css"; // Make sure to create a CSS file for styling
 
 const TiptapEditor = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
       Image,
-      Underline,
+      Link,
       TextAlign.configure({
         types: ["heading", "paragraph"],
       }),
@@ -76,9 +30,62 @@ const TiptapEditor = () => {
     content: "<p>Hello World!</p>",
   });
 
+  const addImage = () => {
+    const url = window.prompt("Enter the image URL");
+    if (url && editor) {
+      editor.chain().focus().setImage({ src: url }).run();
+    }
+  };
+
+  if (!editor) {
+    return null;
+  }
+
   return (
-    <div>
-      <MenuBar editor={editor} />
+    <div className="editor-container">
+      <div className="editor-toolbar">
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is-active" : ""}
+        >
+          <FaBold />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") ? "is-active" : ""}
+        >
+          <FaItalic />
+        </button>
+        {/* <button
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          className={editor.isActive("underline") ? "is-active" : ""}
+        >
+          <FaUnderline />
+        </button> */}
+        <button onClick={addImage}>
+          <FaImage />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("left").run()}
+          className={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}
+        >
+          <FaAlignLeft />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("center").run()}
+          className={
+            editor.isActive({ textAlign: "center" }) ? "is-active" : ""
+          }
+        >
+          <FaAlignCenter />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("right").run()}
+          className={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}
+        >
+          <FaAlignRight />
+        </button>
+      </div>
       <EditorContent editor={editor} />
     </div>
   );
