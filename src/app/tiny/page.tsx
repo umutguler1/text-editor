ca06adfad04c33ee148e5c6f1e74c754cd5f47de
Umut Güler <umtglr1121@gmail.com>
2024-05-21 11:06:31 +0300

    tiny file upload with browse

--- src/app/tiny/page.tsx
+++ src/app/tiny/page.tsx
@@ -4,18 +4,19 @@ import { Editor } from "@tinymce/tinymce-react";
 import React, { useRef } from "react";
 
 const Tiny = () => {
-  const editorRef = useRef(null);
+  const editorRef = useRef<any>(null);
+
   const log = () => {
     if (editorRef.current) {
-      // @ts-ignore
       console.log(editorRef.current.getContent());
     }
   };
+
   return (
     <>
       <Editor
         apiKey="0y7q4g41x8vdp10hbci123qkbl0juzk2ixyc9uby7xfs9u14"
-        onInit={(_evt: any, editor: any) => (editorRef.current = editor)}
+        onInit={(_evt, editor) => (editorRef.current = editor)}
         initialValue="<p>This is the initial content of the editor.</p>"
         init={{
           height: 500,
@@ -47,6 +48,35 @@ const Tiny = () => {
             "removeformat | help | image media",
           content_style:
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          file_picker_callback: (cb, value, meta) => {
+            const input = document.createElement("input");
+            input.setAttribute("type", "file");
+            input.setAttribute("accept", "image/*");
+
+            input.addEventListener("change", (e) => {
+              const file = (e.target as HTMLInputElement).files?.[0];
+
+              if (file) {
+                const reader = new FileReader();
+                reader.addEventListener("load", () => {
+                  if (editorRef.current) {
+                    const id = "blobid" + new Date().getTime();
+                    const blobCache = editorRef.current.editorUpload.blobCache;
+                    const base64 = (reader.result as string).split(",")[1];
+                    const blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+
+                    cb(blobInfo.blobUri(), { title: file.name });
+
+                    // SAVE THE FILE TO A DB
+                  }
+                });
+                reader.readAsDataURL(file);
+              }
+            });
+
+            input.click();
+          },
         }}
       />
       <button onClick={log}>Log editor content</button>
